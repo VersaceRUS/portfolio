406f9d967c47786f5f165f5c0f0093fecddfa386
VersaceRUS <82709245+VersaceRUS@users.noreply.github.com>
2022-07-01 03:17:41 +0300

    Add files via upload

--- style.js
+++ style.js
@@ -32,10 +32,41 @@ document.querySelector('.portf-me').addEventListener('click', () => {
   swiper.slideTo(3);
 });
 
+//--HEADER
 
 document.querySelectorAll('.header-list > li > button').forEach((n, i, a) => {
   n.addEventListener('click', () => a.forEach(m => m.classList.toggle('active', m === n)));
 });
+
+
+//--ABOUT
+
+document.querySelectorAll('.about-list > li').forEach((n, i, a) => {
+  n.addEventListener('click', () => a.forEach(m => m.classList.toggle('active', m === n)));
+});
+
+let selectors = document.querySelectorAll('.about-item'),
+    blocks = document.querySelectorAll('.about-sub');
+
+Array.prototype.forEach.call(selectors, function(selector, index) {
+  selector.addEventListener('click', function(evt){
+    evt.preventDefault();
+    clickButtonHandler(blocks[index])
+  })
+})
+
+function clickButtonHandler(currentBlock) {
+
+  Array.prototype.forEach.call(blocks, function(block) {
+    if (block.classList.contains('opened')) {
+      block.classList.remove('opened');
+    }
+  })
+  currentBlock.classList.add('opened')
+}
+
+
+//--SKULLS
 
 document.addEventListener('DOMContentLoaded', function() {
    let modalButtons = document.querySelectorAll('.js-open-modal'),
@@ -63,27 +94,4 @@ document.addEventListener('DOMContentLoaded', function() {
    });
 });
 
-document.querySelectorAll('.about-list > li').forEach((n, i, a) => {
-  n.addEventListener('click', () => a.forEach(m => m.classList.toggle('active', m === n)));
-});
 
-var selectors = document.querySelectorAll('.about-item'),
-    blocks = document.querySelectorAll('.about-sub');
-
-Array.prototype.forEach.call(selectors, function(selector, index) {
-  selector.addEventListener('click', function(evt){
-    evt.preventDefault();
-    clickButtonHandler(blocks[index])
-
-  })
-})
-
-function clickButtonHandler(currentBlock) {
-
-  Array.prototype.forEach.call(blocks, function(block) {
-    if (block.classList.contains('opened')) {
-      block.classList.remove('opened');
-    }
-  })
-  currentBlock.classList.add('opened')
-}
